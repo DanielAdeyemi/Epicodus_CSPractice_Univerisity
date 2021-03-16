1bcb08b67ad837246c280ca3e3aeb2d68ad3b787
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-16 13:58:57 -0700

    create designtime file

---
+++ University/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace University.Models
+{
+  public class UniversityContextFactory : IDesignTimeDbContextFactory<UniversityContext>
+  {
+    UniversityContext IDesignTimeDbContextFactory<UniversityContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsetting.json")
+        .Build();
+      var builder = new DbContextOptionsBuilder<UniversityContext>();
+      builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
+      return new UniversityContext(builder.Options);
+    }
+  }
+}
